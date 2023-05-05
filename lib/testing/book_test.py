2997d551c3b2362cca1508e86e9de8725f58568a
DavidWolff218 <dcwolff218@gmail.com>
2023-05-05 13:27:27 -0500

    rewrote python test so no default page_count needed

--- lib/testing/book_test.py
+++ lib/testing/book_test.py
@@ -8,46 +8,48 @@ import sys
 class TestBook:
     '''Book in book.py'''
 
-    def test_initialize_with_title(self):
-        '''gets initialized with a title.'''
-        Book("And Then There Were None")
+    # def test_initialize_with_title(self):
+    #     '''gets initialized with a title.'''
+    #     Book("And Then There Were None")
 
-    def test_has_title(self):
-        '''has the title passed into __init__.'''
-        book = Book("And Then There Were None")
+    def test_has_title_and_page_count(self):
+        '''has the title and page_count passed into __init__.'''
+        book = Book("And Then There Were None", 272)
+        # book.page_count = 272
+        assert book.page_count == 272
         assert(book.title == "And Then There Were None")
 
     def test_has_author_name(self):
         '''can be assigned an author name.'''
-        book = Book("And Then There Were None")
+        book = Book("And Then There Were None", 272)
         book.author = "Agatha Christie"
         assert(book.author == "Agatha Christie")
 
-    def test_has_page_count(self):
-        '''can be assigned a page count property.'''
-        book = Book("And Then There Were None")
-        book.page_count = 272
-        assert book.page_count == 272
+    # def test_has_page_count(self):
+    #     '''can be assigned a page count property.'''
+    #     book = Book("And Then There Were None")
+    #     book.page_count = 272
+    #     assert book.page_count == 272
 
     def test_requires_int_page_count(self):
         '''prints "page_count must be an integer" if page_count is not an integer.'''
-        book = Book("And Then There Were None")
+        book = Book("And Then There Were None", 272)
         captured_out = io.StringIO()
         sys.stdout = captured_out
         book.page_count = "not an integer"
         sys.stdout = sys.__stdout__
         assert captured_out.getvalue() == "page_count must be an integer\n"
-        assert not book.page_count
+        # assert not book.page_count
 
     def test_has_genre(self):
         '''can be assigned a genre.'''
-        book = Book("And Then There Were None")
+        book = Book("And Then There Were None", 272)
         book.genre = "Mystery"
         assert(book.genre == "Mystery")
 
     def test_can_turn_page(self):
         '''outputs "Flipping the page...wow, you read fast!" when method turn_page() is called'''
-        book = Book("The World According to Garp")
+        book = Book("The World According to Garp", 69)
         captured_out = io.StringIO()
         sys.stdout = captured_out
         book.turn_page()
