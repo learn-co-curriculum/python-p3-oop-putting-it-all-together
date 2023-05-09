93175fd79a209888643fabfa73e790d9ad8ed723
DavidWolff218 <dcwolff218@gmail.com>
2023-05-09 09:57:05 -0500

    deleted solution code in book and shoe for PR

--- lib/book.py
+++ lib/book.py
@@ -1,53 +1,6 @@
 #!/usr/bin/env python3
 
 class Book:
+    pass
+    
         
-    def __init__(self, title, page_count):
-        
-       self.title = title 
-       self.page_count = page_count
-       
-    def get_page_count(self):
-        print("getting page count")
-        return self._page_count
-    
-    def set_page_count(self, page_count):
-        if type(page_count) == int:
-            print("setting page_count")
-            self._page_count = page_count
-        else:
-            print("page_count must be an integer")
-            
-    def turn_page(self):
-        print("Flipping the page...wow, you read fast!")
-       
-    page_count = property(get_page_count, set_page_count)
-    
-          
-  
-    # def int_page_count(page_count):
-    #     if(isinstance(page_count, int)) :
-    #         return page_count
-    #     else :
-    #         print("page_count must be an integer")
-    
-    
-    # def __init__(self, title, page_count=0):
-    #     self.title = title
-    #     self.page_count = page_count
-
-    # def turn_page(self):
-    #     print("Flipping the page...wow, you read fast!")
-
-    # def get_page_count(self):
-    #     print("running get_page")
-    #     return self._page_count
-
-    # def set_page_count(self, value):
-    #     print("running set_page")
-    #     if type(value) == int:
-    #         self._page_count = value
-
-    #     print("page_count must be an integer")
-    # print("about to hit property")
-    # page_count = property(get_page_count, set_page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,22 +1,4 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    
-    def __init__(self, brand, size):
-        self.brand = brand
-        self.size = size
-        
-    def get_size(self):
-        return self._size
-    
-    def set_size(self, size):
-        if type(size)  == int:
-            self._size = size
-        else:
-            print("size must be an integer")
-            
-    def cobble(self):
-        self.condition = "New"
-        print('Your shoe is as good as new!')
-            
-    size = property(get_size, set_size)
+    pass
