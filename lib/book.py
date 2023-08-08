77993a3623bd9125c7ce1195927066013cc15352
Oli Treadwell <oliver.treadwell@gmail.com>
2023-08-08 14:25:07 -0700

    add solution based on properties lesson

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        page_count_type = type(page_count)
+        if page_count_type == int:
+            self._page_count = page_count
+        if page_count_type != int:
+            print('page_count must be an integer')
+
+
+    page_count = property(get_page_count, set_page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        size_type = type(size)
+        if size_type == int:
+            self._size = size
+        if size_type != int:
+            print('size must be an integer')
+
+
+    size = property(get_size, set_size)
